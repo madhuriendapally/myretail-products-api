e3dbe7a5bd2fd000e6837628af10149615eda985
madhuriendapally <kits.madhu@gmail.com>
2022-07-27 00:23:32 -0500

    Integration with RedSky service

---
+++ src/main/java/myretail/products/api/demo/config/WebClientConfiguration.java
@@ -0,0 +1,24 @@
+package myretail.products.api.demo.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Data
+@Configuration
+@ConfigurationProperties("redsky.api")
+public class WebClientConfiguration {
+    private String url;
+
+    @Bean("redskyWebClient")
+    public WebClient webClient() {
+        return WebClient.builder()
+                .baseUrl(url)
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+    }
+}
